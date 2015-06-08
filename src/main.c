c1d73d4e8269778b2f9e530e56ccb05a9160bc2c
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2015-06-08 08:37:10 +0000

    Loop while doing something to waste CPU

--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
+/**
+ * Reference: http://www.thegeekstuff.com/2012/03/linux-threads-intro/
+ **/
+
 #include <stdio.h>
+#include <time.h>
 
 main(int argc, char *argv[])
 {
-    printf("Hello, world!\n");
+    int count = 0;
+    struct timespec req;
+    struct timespec rem;
+
+    printf("DEBUG: hello-thread start\n");
+
+    req.tv_sec = (time_t)0;
+    req.tv_nsec = 1000000l; /* 1 ms */
+
+    while (1) {
+        printf("main: count=%d\n", count);
+        //sleep(10);
+        nanosleep(&req, &rem);
+        count++;
+    }
+
+    printf("DEBUG: hello-thread stop\n");
     return 0;
 }
 
